0ead9b7afbcdf43c8e00eedb2689335e669da1e3
marciogameiro <marciogameiro@gmail.com>
2025-04-28 11:53:11 -0400

    Add hysteresis computation for DSGRN blowup graph

--- Hysteresis_Paths/HysteresisPathsBlowup.py
+++ Hysteresis_Paths/HysteresisPathsBlowup.py
@@ -1,122 +1,120 @@
-# HystereticPaths.py
-# Marcio Gameiro
-# 2020-10-18
-# MIT LICENSE
+### HysteresisPathsBlowup.py
+### MIT LICENSE 2025 Marcio Gameiro
 
 import DSGRN
+import DSGRN_utils
 import itertools
 import functools
+import sys
 
 class NetworkAnalyzer:
-    def __init__(self, network, P_gene):
+    def __init__(self, network, P_gene, level):
         self.network = network
         # Signal output gene index
         self.P_index = network.index(P_gene)
+        self.level = level
+        self.dim = self.network.size()
         self.parameter_graph = DSGRN.ParameterGraph(network)
 
-    def is_FP(self, annotation):
-        return annotation.startswith("FP")
+    def classify_FP(self, morse_node, graded_complex):
+        """Classify FP as quiescent or proliferative"""
+        # Get cells in the Morse set. The Morse graph vertex (morse_node) is the cell grading
+        morse_set = [c for c in graded_complex.complex()(self.dim) if graded_complex.value(c) == morse_node]
+        if len(morse_set) != 1:
+            return 'O'
+        # Get cell coordinates in the cubical blowup complex
+        cell_coords = graded_complex.complex().coordinates(morse_set[0])
+        # Cells with 0 coordinate are boundary cells
+        # Quiescent if lowest non-boundary coordinate
+        if cell_coords[self.P_index] == 1:
+            return 'q' # Quiescent
+        # Proliferative if not lowest non-boundary coordinate
+        if cell_coords[self.P_index] > 1:
+            return 'p' # Proliferative
+        return 'O'
 
-    def is_quiescent_FP(self, annotation):
-        if self.is_FP(annotation):
-            digits = [int(s) for s in annotation.replace(",", "").split() if s.isdigit()]
-            if digits[self.P_index] == 0:
-                return True
-        return False
+    # def quiescent_FP(self, morse_node, graded_complex):
+    #     # Get cells in the Morse set. The Morse graph vertex (morse_node) is the cell grading
+    #     morse_set = [c for c in graded_complex.complex()(self.dim) if graded_complex.value(c) == morse_node]
+    #     if len(morse_set) != 1:
+    #         return False
+    #     # Get cell coordinates in the cubical blowup complex
+    #     cell_coords = graded_complex.complex().coordinates(morse_set[0])
+    #     # Cells with 0 coordinate are boundary cells
+    #     # Check if lowest non-boundary coordinate
+    #     if cell_coords[self.P_index] == 1:
+    #         return True
+    #     return False
 
-    def is_proliferative_FP(self, annotation):
-        if self.is_FP(annotation):
-            digits = [int(s) for s in annotation.replace(",", "").split() if s.isdigit()]
-            if digits[self.P_index] > 0:
-                return True
-        return False
+    # def proliferative_FP(self, morse_node, graded_complex):
+    #     # Get cells in the Morse set. The Morse graph vertex (morse_node) is the cell grading
+    #     morse_set = [c for c in graded_complex.complex()(self.dim) if graded_complex.value(c) == morse_node]
+    #     if len(morse_set) != 1:
+    #         return False
+    #     # Get cell coordinates in the cubical blowup complex
+    #     cell_coords = graded_complex.complex().coordinates(morse_set[0])
+    #     # Cells with 0 coordinate are boundary cells
+    #     # Check if not lowest non-boundary coordinate
+    #     if cell_coords[self.P_index] > 1:
+    #         return True
+    #     return False
 
     def ClassifyParameter(self, par_index):
         parameter = self.parameter_graph.parameter(par_index)
-        domain_graph = DSGRN.DomainGraph(parameter)
-        morse_decomp = DSGRN.MorseDecomposition(domain_graph.digraph())
-        morse_graph = DSGRN.MorseGraph(domain_graph, morse_decomp)
-        morse_graph_poset = morse_graph.poset()
-        stable_annotations = [morse_graph.annotation(n)[0] for n in range(morse_graph.poset().size()) if len(morse_graph.poset().children(n)) == 0]
-        monostable = len(stable_annotations) == 1
-        quiescent = any(self.is_quiescent_FP(annotation) for annotation in stable_annotations)
-        proliferative = any(self.is_proliferative_FP(annotation) for annotation in stable_annotations)
-        if monostable and quiescent:
+        morse_graph, stg, graded_complex = DSGRN_utils.ConleyMorseGraph(parameter, level=self.level)
+        conley_index = lambda node: morse_graph.vertex_label(node).split(':')[1].strip()
+        # conley_index = lambda node: eval(morse_graph.vertex_label(node).split(':')[1].strip())
+        FP_conley_index = lambda node: conley_index(node).count('1') == 1
+        stable_nodes = [node for node in morse_graph.vertices() if len(morse_graph.adjacencies(node)) == 0]
+        stable_FPs = [node for node in stable_nodes if FP_conley_index(node)]
+        stable_FP_types = [self.classify_FP(node, graded_complex) for node in stable_FPs]
+        quiescent = any(type == 'q' for type in stable_FP_types)
+        proliferative = any(type == 'p' for type in stable_FP_types)
+        if len(stable_FPs) == 1 and quiescent:
+            # Quiescent and monostable
             return 'Q'
-        if monostable and proliferative:
+        if len(stable_FPs) == 1 and proliferative:
+            # Proliferative and monostable
             return 'P'
         if quiescent and proliferative:
+            # Quiescent and proliferative
             return 'B'
-        if quiescent:
-            return 'q'
-        if proliferative:
-            return 'p'
         return 'O'
 
-    def is_hysteretic_path(self, path, hyst_type='ascending'):
-        # Recalling that the labels are:
+    def is_hysteretic_path(self, path, strict=True, hyst_type='ascending'):
+        # The labels are:
         # 'Q' : Quiescent and monostable
-        # 'q' : Quiescent but not monostable
         # 'B' : Quiescent and proliferative (at least bi-stable)
         # 'P' : Proliferative and monostable
-        # 'p' : Proliferative but not monostable
-        # A path is defined as hysteretic if the following are true:
-        # 1) The path starts with a 'Q'
-        # 2) Followed by one or more 'Q' or 'q'
-        # 3) Followed by one or more 'B'
-        # 4) Followed by one or more 'P' or 'p'
-        # 5) The path ends with a 'P'
-
-        # Get classification label for each parameter in path
-        labels = [self.ClassifyParameter(par_index) for par_index in path]
-        # Transform labels into string
-        path_str = ''.join(labels)
-        if hyst_type == 'ascending':
-            # Return false if does not start with 'Q'
-            if not path_str.startswith('Q'):
-                return False
-            # Return false if does not end with 'P'
-            if not path_str.endswith('P'):
-                return False
-        else: # Assume type == 'descending'
-            # Return false if does not start with 'P'
-            if not path_str.startswith('P'):
-                return False
-            # Return false if does not end with 'Q'
-            if not path_str.endswith('Q'):
-                return False
-        # Replace 'q' by 'Q' and 'p' by 'P'
-        path_str = path_str.replace('q', 'Q')
-        path_str = path_str.replace('p', 'P')
-        # Remove repeated consecutive labels from string
-        path_str = ''.join(key for key, group in itertools.groupby(list(path_str)))
-        if hyst_type == 'ascending':
-            # Path is hysteretic iff path_str == 'QBP'
-            return path_str == 'QBP'
-        else: # Assume type == 'descending'
-            # Path is hysteretic iff path_str == 'PBQ'
-            return path_str == 'PBQ'
-
-    def is_hysteretic_path_strict(self, path, hyst_type='ascending'):
-        # A more strict notion of hysteresis is to require:
+        #
+        # A path exhibits (ascending) hysteresis if:
         # 1) The path starts with one or more 'Q'
         # 2) Followed by one or more 'B'
         # 3) The path ends with one or more 'P'
-
-        # Get classification label for each parameter in path
+        #
+        # A more strict notion of (ascending) hysteresis is:
+        # 1) The path starts with a single 'Q'
+        # 2) Followed by one or more 'B'
+        # 3) The path ends with a single 'P'
+        #
+        # Get classification labels for each parameter in path
         labels = [self.ClassifyParameter(par_index) for par_index in path]
+        # If strict make sure a single Q and a single P
+        if strict and labels.count('Q') != 1:
+            return False
+        if strict and labels.count('P') != 1:
+            return False
         # Remove repeated consecutive labels and make a string
         path_str = ''.join(key for key, group in itertools.groupby(labels))
         if hyst_type == 'ascending':
             # Path is hysteretic iff path_str == 'QBP'
             return path_str == 'QBP'
-        else: # Assume type == 'descending'
-            # Path is hysteretic iff path_str == 'PBQ'
-            return path_str == 'PBQ'
+        # Else assume type == 'descending'
+        # Path is hysteretic iff path_str == 'PBQ'
+        return path_str == 'PBQ'
 
 def reverse_topological_sort(graph):
-    """Return list of vertices in reverse topologically sorted order.
-    """
+    """Return list of vertices in reverse topologically sorted order."""
     result = []
     explored = set()
     dfs_stack = [(v,0) for v in graph.vertices]
@@ -141,7 +139,8 @@ def get_all_paths(graph, source=None, target=None, allowed=None):
     top_sorted_verts = reverse_topological_sort(graph)
     paths = {} # Allowed paths from a vertex v to a taget vertex
     result = []
-    for v in top_sorted_verts:
+    num_verts = len(top_sorted_verts)
+    for k, v in enumerate(top_sorted_verts):
         if not allowed(v):
             continue
         paths[v] = [[v] + path for u in graph.adjacencies(v) for path in paths[u] if allowed(u)]
@@ -149,9 +148,11 @@ def get_all_paths(graph, source=None, target=None, allowed=None):
             paths[v].append([v])
         if source(v):
             result.extend([path for path in paths[v] if len(path) > 2])
+        sys.stdout.write(f'\rComputing list of factor graph paths: {(k + 1) / num_verts:0.1%} complete.')
+    sys.stdout.write(f'\rComputing list of factor graph paths: {num_verts / num_verts:0.1%} complete.\n')
     return result
 
-def compute_hysteresis_paths(network, S_gene, P_gene, rpi, strict=True, hyst_type='ascending'):
+def compute_hysteresis_paths(network, S_gene, P_gene, rpi, strict=True, hyst_type='ascending', level=3):
     """Given a network, a input gene name (S_gene), a output
     gene name (P_gene) and a relative parameter index (rpi)
     compute all hysteretic paths.
@@ -193,20 +194,19 @@ def compute_hysteresis_paths(network, S_gene, P_gene, rpi, strict=True, hyst_typ
                                                    (pi // indexing_place_values[S_index]) % \
                                                    indexing_place_bases[S_index])
     # Set network analyzer
-    network_analyzer = NetworkAnalyzer(network, P_gene) 
+    network_analyzer = NetworkAnalyzer(network, P_gene, level)
     # Get all paths in S_gene factor graph
     factor_paths = get_all_paths(factor_graph)
     # Get hysteresis paths
-    hysteretic_paths = []
-    for factor_path in factor_paths:
+    hysteresis_paths = []
+    num_factor_paths = len(factor_paths)
+    for k, factor_path in enumerate(factor_paths):
         # Get path with indices in full indices
         path = [full_parameter_index(rpi, gpi, S_index) for gpi in factor_path]
-        if strict:
-            if network_analyzer.is_hysteretic_path_strict(path, hyst_type):
-                hysteretic_paths.append(path)
-        else:
-            if network_analyzer.is_hysteretic_path(path, hyst_type):
-                hysteretic_paths.append(path)
+        if network_analyzer.is_hysteretic_path(path, strict=strict, hyst_type=hyst_type):
+            hysteresis_paths.append(path)
+        sys.stdout.write(f'\rAnalyzing factor graph paths for hysteresis: {(k + 1) / num_factor_paths:0.1%} complete.')
+    sys.stdout.write(f'\rAnalyzing factor graph paths for hysteresis: {num_factor_paths / num_factor_paths:0.1%} complete.\n')
     # Get the total number of paths
     num_paths = len(factor_paths)
-    return num_paths, hysteretic_paths
+    return num_paths, hysteresis_paths
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # hysteresis-paths
 
-Compute hysteretic paths for a give network. See the Jupyter notebooks for examples of how to compute and read the data.
+Compute hysteresis paths for a given network. See the Jupyter notebooks for examples of how to compute and read the data.
 
 This code computes all paths and return the ones that are hysteretic. It is intended to be used only for small networks since the number of paths for larger networks can be extremely large.
